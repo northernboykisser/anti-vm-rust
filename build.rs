9824ef66962e1c0572417f978234e4c7a13857e6
si11ykat <northernboykisser@gmail.com>
2026-08-11 21:55:35 +0300

    Update build.rs

--- build.rs
+++ build.rs
@@ -12,11 +12,9 @@ fn main() {
     println!("cargo:warning=WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ!");
     println!("cargo:warning=WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ!");
     println!("cargo:warning=WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ!");
-    println!("cargo:warning=WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ!");
-    println!("cargo:warning=WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ!");
     println!("cargo:warning=WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ!");            
-    println!("cargo:warning=ПРИВЕТ! Библиотека ANTIVM обновлена! Пожалуйста ознакомтесь с новвоведениями:");
-    println!("cargo:warning=Hi! The ANTIVM library has been updated! Please take a look at the new features:");
-    println!("https://github.com/northernboykisser/anti-vm-rust/blob/master/updated.md");
+    println!("cargo:warning=ПРИВЕТ! Библиотека ANTIVM обновлена! Пожалуйста ознакомтесь с новвоведениями: github.com/northernboykisser/anti-vm-rust/blob/master/updated.md");
+    println!("cargo:warning=Hi! The ANTIVM library has been updated! Please take a look at the new features: github.com/northernboykisser/anti-vm-rust/blob/master/updated.md");
+    println!(">> github.com/northernboykisser/anti-vm-rust/blob/master/updated.md");
 }
 
