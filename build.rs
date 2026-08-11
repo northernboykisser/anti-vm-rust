431ace3a414eaa0da1b141b1b6dd0d3a5156f698
si11ykat <northernboykisser@gmail.com>
2026-08-11 21:54:39 +0300

    build.rs msg updated

--- build.rs
+++ build.rs
@@ -17,5 +17,6 @@ fn main() {
     println!("cargo:warning=WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ!");            
     println!("cargo:warning=ПРИВЕТ! Библиотека ANTIVM обновлена! Пожалуйста ознакомтесь с новвоведениями:");
     println!("cargo:warning=Hi! The ANTIVM library has been updated! Please take a look at the new features:");
+    println!("https://github.com/northernboykisser/anti-vm-rust/blob/master/updated.md");
 }
 
