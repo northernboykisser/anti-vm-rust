0699bebced37f01de24ea5afd012b88fe81a6781
si11ykat <northernboykisser@gmail.com>
2026-08-12 21:05:07 +0300

    edit welcome msg

--- build.rs
+++ build.rs
@@ -1,20 +1,13 @@
-// build.rs
-fn main() {
-    println!("cargo:warning=WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ!");
-    println!("cargo:warning=WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ!");
-    println!("cargo:warning=WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ!");
-    println!("cargo:warning=WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ!");
-    println!("cargo:warning=WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ!");
-    println!("cargo:warning=WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ!");
-    println!("cargo:warning=WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ!");
-    println!("cargo:warning=WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ!");
-    println!("cargo:warning=WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ!");
-    println!("cargo:warning=WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ!");
-    println!("cargo:warning=WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ!");
-    println!("cargo:warning=WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ!");
-    println!("cargo:warning=WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ! WARNING! ВНИМАНИЕ!");            
-    println!("cargo:warning=ПРИВЕТ! Библиотека ANTIVM обновлена! Пожалуйста ознакомтесь с новвоведениями: github.com/northernboykisser/anti-vm-rust/blob/master/updated.md");
-    println!("cargo:warning=Hi! The ANTIVM library has been updated! Please take a look at the new features: github.com/northernboykisser/anti-vm-rust/blob/master/updated.md");
-    println!(">> github.com/northernboykisser/anti-vm-rust/blob/master/updated.md");
-}
+use winapi::um::winuser::*;
 
+fn main() {
+    if std::env::var("CARGO_FEATURE_SHOW_NOTIFICATION").is_err() {
+        return; 
+    }
+
+    unsafe {
+        let title: Vec<u16> = "msg at developer\0".encode_utf16().collect();
+        let text: Vec<u16> = "Thanks for using lib 'antivm'\nСпасибо за использование библиотеки 'antivm'\n\n\nmy github: github.com/northernboykisser\nmy telegram: @nrthbkser\n\n\nIf you don't want to see this, modify Cargo.toml.\nЕсли не хотите видеть это измените cargo.toml\n:\nantivm = { version = ver, default-features = false }\0".encode_utf16().collect();
+        MessageBoxW(std::ptr::null_mut(), text.as_ptr(), title.as_ptr(), MB_OK);
+    }
+}
